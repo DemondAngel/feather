ca117b444863d8cd25f29df8e67e186e08ff2051
Emme243 <emmlop243@gmail.com>
2021-07-05 22:15:57 -0500

    Refactorizacion de GraphQl y arreglo de bootstrap

--- api-feather/frontend/nuxt.config.js
+++ api-feather/frontend/nuxt.config.js
@@ -25,7 +25,7 @@ export default {
   css: ['~/assets/css/main.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [{ src: '~/plugins/vuelidate' }],
+  plugins: [{ src: '~/plugins/vuelidate' }, { src: '~/plugins/bootstrap.js' }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
